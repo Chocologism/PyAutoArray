e512a6b9e9194b1310b8963979e3929168096f6f
dinghengkai_superpmo <dinghengkai22@mails.ucas.ac.cn>
2025-12-07 23:24:41 +0800

    merge Hilbert_Circular with origianl Hilbert

--- autoarray/inversion/pixelization/image_mesh/__init__.py
+++ autoarray/inversion/pixelization/image_mesh/__init__.py
@@ -1,4 +1,4 @@
-from .hilbert import Hilbert, Hilbert_Circular, Hilbert_Annular
+from .hilbert import Hilbert, Hilbert_Annular
 from .overlay import Overlay
 from .kmeans import KMeans
 from .abstract import append_with_circle_edge_points
